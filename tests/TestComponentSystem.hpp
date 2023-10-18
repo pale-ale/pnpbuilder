e7e09ad20cd612c6c7ee2c29f1d6b9e93b83ef8e
pale-ale <alex.baucke@tutanota.com>
2023-10-18 21:31:33 +0200

    add tests for component system

--- tests/TestComponentSystem.hpp
+++ tests/TestComponentSystem.hpp
@@ -1,7 +1,47 @@
+#include "../src/Components/GraphicsComponent.hpp"
+#include "../src/Entities/Entity.hpp"
 #include <QTest>
 
 class TestComponentSystem : public QObject {
   Q_OBJECT
 private slots:
-  void doStuff() { QVERIFY(1 == 2); }
+  void testComponentCreation() {
+    Entity *entity = new Entity("testEntity");
+    auto componentCount = [entity]() { return entity->components.size(); };
+    QVERIFY(componentCount() == 0);
+
+    entity->addComponent(new ComponentBase());
+    QVERIFY(componentCount() == 1);
+
+    entity->addComponent(new GraphicsComponent());
+    QVERIFY(componentCount() == 2);
+  }
+
+  void testComponentAccess() {
+    Entity *entity = new Entity("abc123");
+    {
+      auto component = entity->getComponentOfType<ComponentBase>();
+      QVERIFY(component == nullptr);
+      component = entity->getComponentOfType<GraphicsComponent>();
+      QVERIFY(component == nullptr);
+    }
+    entity->addComponent(new ComponentBase());
+    {
+      auto component = entity->getComponentOfType<ComponentBase>();
+      QVERIFY(component != nullptr);
+      component = entity->getComponentOfType<GraphicsComponent>();
+      QVERIFY(component == nullptr);
+    }
+    entity->addComponent(new GraphicsComponent());
+    {
+      auto component = entity->getComponentOfType<ComponentBase>();
+      QVERIFY(component != nullptr);
+      component = entity->getComponentOfType<GraphicsComponent>();
+      QVERIFY(component != nullptr);
+    }
+    QVERIFY(typeid(*entity->components[0]) == typeid(ComponentBase));
+    QVERIFY(typeid(*entity->components[0]) != typeid(GraphicsComponent));
+    QVERIFY(typeid(*entity->components[1]) != typeid(ComponentBase));
+    QVERIFY(typeid(*entity->components[1]) == typeid(GraphicsComponent));
+  }
 };
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,4 +1,13 @@
 #include "TestComponentSystem.hpp"
 #include <QTest>
 
-QTEST_MAIN(TestComponentSystem)
+int main(int argc, char *argv[]) {
+  auto runTest = [&argc, &argv](QObject *test) {
+    return QTest::qExec(test, argc, argv);
+  };
+
+  int status = 0;
+  status |= runTest(new TestComponentSystem);
+
+  return status;
+}
